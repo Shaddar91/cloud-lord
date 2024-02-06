961f0cbbddae1f12586e8cfe4ec8545278c4c1f1
Shaddar91 <shaddar45@gmail.com>
2024-02-06 19:44:04 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
-
 import React from 'react';
+import { Helmet } from 'react-helmet'; // Import Helmet for SEO
 import { Element, Link } from 'react-scroll';
 import About from './pages/About';
 import Services from './pages/Services';
@@ -25,21 +25,27 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+
+      {/* Helmet for SEO */}
+      <Helmet>
+        <title>Cloud Lord - Your Cloud Infrastructure Partner</title>
+        <meta name="description" content="Cloud Lord provides expert cloud infrastructure and DevOps services tailored to your needs." />
+        <meta name="keywords" content="cloud services, DevOps, cloud computing, infrastructure as code, terraform, kubernetes" />
+      </Helmet>
+
       <AppBar position="static" sx={{ backgroundColor: 'rgba(0, 0, 0, 1)', color: '#fff' }}>
         <Toolbar>
           <img src={logo} alt="Logo" style={{ height: '80px' }} />
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Cloud Lord
           </Typography>
-          {/* Adjust the navigation to include "About" and "Services" and ensure they are white */}
           <Button color="inherit">
             <Link to="about" spy={true} smooth={true} duration={500} style={{ color: '#fff' }}>About</Link>
           </Button>
           <Button color="inherit">
             <Link to="services" spy={true} smooth={true} duration={500} style={{ color: '#fff' }}>Services</Link>
           </Button>
-          {/* Add icon buttons for LinkedIn and GitHub */}
-          <IconButton color="inherit" href="https://rs.linkedin.com/in/tomislav-ivanovic-124b13115" target="_blank">
+          <IconButton color="inherit" href="https://www.linkedin.com/in/tomislav-ivanovic-124b13115" target="_blank">
             <LinkedInIcon />
           </IconButton>
           <IconButton color="inherit" href="https://github.com/Shaddar91" target="_blank">
@@ -47,9 +53,8 @@ function App() {
           </IconButton>
         </Toolbar>
       </AppBar>
-      
+
       <div className="App-content">
-        {/* Scrollable sections for About and Services */}
         <Element name="about" className="element">
           <About />
         </Element>
